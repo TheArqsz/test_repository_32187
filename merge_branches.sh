10f43597c0686168390a6e8882f88a3b7a706456
TheArqsz <arek.maruszczak@gmail.com>
2020-05-28 16:20:15 +0200

    fix

--- merge_branches.sh
+++ merge_branches.sh
@@ -9,26 +9,28 @@ fi
 export GIT_COMMITTER_EMAIL='travis@travis'
 export GIT_COMMITTER_NAME='Travis CI'
 export GITHUB_REPO='TheArqsz/AntiPhishMe-backend'
+export GIT_BRANCH_FROM='develop'
+export GIT_BRANCH_TO='master'
 
-if [ $TRAVIS_BRANCH != "develop" ]; then
+if [ $TRAVIS_BRANCH != $GIT_BRANCH_FROM ]; then
     echo "Current branch doesn't match requirements to be merged"
     exit 0
 fi
 
 repo_temp=$(mktemp -d)
-git clone "https://github.com/TheArqsz/AntiPhishMe-backend" "$repo_temp"
+git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
 
 cd "$repo_temp"
 
-printf 'Checking out %s\n' "master" >&2
-git checkout "master"
+echo "Checking out $GIT_BRANCH_TO" >&2
+git checkout $GIT_BRANCH_TO
 
-printf 'Merging %s\n' "$TRAVIS_COMMIT" >&2
-git merge --ff-only "$TRAVIS_COMMIT"
+echo "Merging $TRAVIS_COMMIT" >&2
+git merge --squash "$TRAVIS_COMMIT"
+git commit -m "Automerging from $GIT_BRANCH_FROM commit $TRAVIS_COMMIT"
 
-printf 'Pushing to %s\n' "https://github.com/$GITHUB_REPO" >&2
+echo "Pushing to https://github.com/$GITHUB_REPO" >&2
 
 push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO"
 
-git push "$push_uri" "master" >/dev/null 2>&1
-git push "$push_uri" :"$TRAVIS_BRANCH" >/dev/null 2>&1
+git push $push_uri $GIT_BRANCH_TO >/dev/null 2>&1
